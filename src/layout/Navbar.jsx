df8460c2529a2d232d0fa74fbb22b45b353b8185
Cyril Derc <cyril_derc@hotmail.com>
2023-11-02 16:31:55 +0100

    Navbar - création du css

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 // import {Link} from "react-router-dom";
 import Logo from '../assets/Booki.png'
 
+import "../styles/Navbar.css"
+
 const Navbar = () => {
     return (
-        <nav>
-        <div>
-            <img src={Logo} alt="logo_de_Booki"/>
+        <nav className='navbar'>
+        <div className='navbar__logo'>
+            <img src={Logo} className='navbar__img' alt="logo_de_Booki"/>
         </div>
          <div className="navbar__links">
          {/* <Link to="/"> */}
