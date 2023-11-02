d8ab7c0a8830924159b3378feb9d32acc19c8ad0
Cyril Derc <cyril_derc@hotmail.com>
2023-11-02 16:19:40 +0100

    Page Home - création des liens

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+// import {Link} from "react-router-dom";
 import Logo from '../assets/Booki.png'
 
 const Navbar = () => {
     return (
+        <nav>
         <div>
             <img src={Logo} alt="logo_de_Booki"/>
         </div>
+         <div className="navbar__links">
+         {/* <Link to="/"> */}
+         <p>Accueil</p>
+         {/* </Link>
+         <Link to="/About"> */}
+         <p>A propos</p>
+         {/* </Link> */}
+       </div>
+        </nav>
     );
 };
 
