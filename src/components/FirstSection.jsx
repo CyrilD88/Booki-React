41108d5a67c4b3a6a929e7374ea06058ae0e43b0
Cyril Derc <cyril_derc@hotmail.com>
2023-11-06 20:07:57 +0100

    FirstSection - création du bouton

--- src/components/FirstSection.jsx
+++ src/components/FirstSection.jsx
@@ -3,8 +3,15 @@ import React from 'react';
 const FirstSection = () => {
     return (
         <div>
-            <p>Trouvez un hébergement pour des vacances de rêve </p>
+            <h1>Trouvez un hébergement pour des vacances de rêve</h1>
             <p>En plein centre-ville ou en pleine nature</p>
+            <form method="post" action="#">               
+                     <i class="fa-solid fa-location-dot"></i>
+                     <input type="text"  placeholder="Marseille, France"/>                
+                    <button><span>Rechercher</span>                                
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                    </button>
+                </form>
         </div>
     );
 };
