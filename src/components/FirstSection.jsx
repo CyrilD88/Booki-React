acd332367d5bde45c29ba63845c0c7a052f0cf67
Cyril Derc <cyril_derc@hotmail.com>
2023-11-07 16:09:19 +0100

    FirstSection mise ne place des icones

--- src/components/FirstSection.jsx
+++ src/components/FirstSection.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHouse} from '@fortawesome/free-solid-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+
+
 
 const FirstSection = () => {
     return (
         <div>
             <h1>Trouvez un hébergement pour des vacances de rêve</h1>
+            <FontAwesomeIcon icon={faEnvelope} />
+            <FontAwesomeIcon icon={faTwitter} />
+            <FontAwesomeIcon icon={faHouse} />
+
             <p>En plein centre-ville ou en pleine nature</p>
             <form method="post" action="#">               
                      <i class="fa-solid fa-location-dot"></i>
-                     <input type="text"  placeholder="Marseille, France"/>                
+                     <input type="text"  placeholder="Marseille, France"/>     
+                          
                     <button><span>Rechercher</span>                                
-                    <i class="fa-solid fa-magnifying-glass"></i>
                     </button>
                 </form>
         </div>
