24a091bc497d658b41af619e00aec696cf9531a3
Cyril Derc <cyril_derc@hotmail.com>
2023-11-10 23:31:32 +0100

    Card - finalisation

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -20,22 +20,19 @@ const Card = () => {
       // affichage de la description pour l'appartement sur la page d'accueil
         return (
             <div className="Logement" key={id}>
-                    <img src={cover} alt={title} />
+                <img src={cover} alt={title} />
+                    <div className='LogementDiv'>
                     <h3>{title}</h3>
                     <p>{description}</p>
                     <div className='stars'>
                         {
                             arrayStars.map(element => {
                                 const nbreEtoiles = parseInt(record.rating);
-                                return(<span key={"star-"} className={element <= nbreEtoiles ? 'span1' : 'span2'}><FontAwesomeIcon icon={faStar} /></span>)
+                                return(<span key={element} className={element <= nbreEtoiles ? 'span1' : 'span2'}><FontAwesomeIcon icon={faStar} /></span>)
                             })
                         }
                     </div>
-                    
-                   
-                
-                    
-             
+                    </div>
             </div>
         )
     })}
