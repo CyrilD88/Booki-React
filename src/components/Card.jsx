0a04da085fd8a3aed1c6abab188c1b7e47ca5a1e
Cyril Derc <cyril_derc@hotmail.com>
2023-11-10 19:36:58 +0100

    Card - étoiles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Data from "../booki.json"
-
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 import '../styles/Card.css'
 
+const arrayStars = [1, 2, 3, 4, 5]
+
+
+
+
 const Card = () => {
+        // On recherche l'id dans le fichier logements.json
+        const record = Data.find(element => element.id)
     return (
         <div className='LogementCard'>
             {Data.map((apart) => {
@@ -16,12 +23,23 @@ const Card = () => {
                     <img src={cover} alt={title} />
                     <h3>{title}</h3>
                     <p>{description}</p>
-
+                    <div className='stars'>
+                        {
+                            arrayStars.map(element => {
+                                const nbreEtoiles = parseInt(record.rating);
+                                return(<span key={"star-"} className={element <= nbreEtoiles ? 'span1' : 'span2'}><FontAwesomeIcon icon={faStar} /></span>)
+                            })
+                        }
+                    </div>
+                    
+                   
+                
+                    
              
             </div>
         )
     })}
-            
+         
         </div>
     );
 };
