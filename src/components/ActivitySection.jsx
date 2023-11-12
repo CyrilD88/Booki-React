16005771e87cac1ccc3525a83f048e681843b85e
Cyril Derc <cyril_derc@hotmail.com>
2023-11-12 16:57:59 +0100

    section activités

---
+++ src/components/ActivitySection.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ActivitySection = () => {
+    return (
+        <div>
+           <div className='activitySection'>
+                <h2>Activités à Marseille</h2>
+                <div className='activitySectionOne'>
+                    <p>hello</p>
+                </div>
+           </div>
+        </div>
+    );
+};
+
+export default ActivitySection;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import FirstSect from "../components/FirstSection"
 import SecondSection from '../components/SecondSection';
+import Activity from '../components/ActivitySection'
 
 const Home = () => {
     return (
       <>
         <FirstSect />
         <SecondSection />
+        <Activity />
       </>
  
      
