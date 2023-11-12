69bcda42a4dc0429d0c5f017a9b58c4e63f779a1
Cyril Derc <cyril_derc@hotmail.com>
2023-11-12 16:37:16 +0100

    SecondCard - mise en forme finalisée

--- src/components/SecondCard.jsx
+++ src/components/SecondCard.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Data from "../bookiPop.json"
 import "../styles/SecondCard.css"
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+
+const arrayStars = [1, 2, 3, 4, 5]
 
 const SecondCard = () => {
+    const record = Data.find(element => element.id)
     return (
         <div className='LogementCardTwo'>
+            
         {Data.map((apart) => {
     const { id, cover, title, description } = apart;
 // test
@@ -14,17 +18,19 @@ const SecondCard = () => {
     return (
         <div className="LogementTwo" key={id}>
             <img src={cover} alt={title} />
-                <div className='LogementDiv'>
-                    <h3>{title}</h3>
-                    <p>{description}</p>
-                {/* <div className='stars'>
+                <div className='LogementDivTwo'>
+                    <div className='LogementDivInfos'>
+                        <h3>{title}</h3>
+                        <p>{description}</p>
+                    </div>
+                    <div className='stars'>
                         {
                             arrayStars.map(element => {
                                 const nbreEtoiles = parseInt(record.rating);
                                 return(<span key={element} className={element <= nbreEtoiles ? 'span1' : 'span2'}><FontAwesomeIcon icon={faStar} /></span>)
                             })
                         }
-                    </div> */}
+                    </div>
                 </div>
             </div>
         )
